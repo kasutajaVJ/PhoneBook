5ddd7c8d8a4a10ad48467d24cd27ea8d0208cb11
vanessa.jalakas <vanessa.jalakas@gmail.com>
2018-10-28 13:48:06 +0200

    Initial commit

---
+++ app/src/main/java/com/nagel/phonebook/Zipcode.java
@@ -0,0 +1,27 @@
+package com.nagel.phonebook;
+
+import java.io.Serializable;
+
+/* zipcode model class, defined Serializable so that objects of the class can be
+ * transferred as parameters to other activities. Class consists of get methods. */
+public class Zipcode implements Serializable {
+
+    private String code, city;
+
+    public Zipcode(String code, String city) {
+        this.code = code;
+        this.city = city;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String toString(){
+        return getCode() + " " + getCity();
+    }
+}
